a867c10ff98ca3f69029796a09bb9406eee0af5d
nickbrannum02 <nickbrannum02@gmail.com>
2023-09-11 19:05:35 -0600

    Uploading screenshots of database completion

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" TEXT   NOT NULL,
+    "last_name" TEXT   NOT NULL,
+    "email" TEXT   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" BIGINT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" TEXT   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" DOUBLE   NOT NULL,
+    "pledged" DOUBLE   NOT NULL,
+    "outcome" TEXT   NOT NULL,
+    "backers_count" BIGINT   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launch_date" TEXT   NOT NULL,
+    "end_date" TEXT   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(15)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
